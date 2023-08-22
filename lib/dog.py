8b394b6109d6668e54d7fdb979b8fc53ffebc341
aluochvivian <aluochviviann@gmail.com>
2023-08-22 09:18:26 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,15 @@
-#!/usr/bin/env python3
-
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+    def sit(self):
+        print("The dog is sitting.")
+
+# Creating instances of the Dog class
+fido = Dog()
+fido.bark()
+fido.sit()
+
+snoopy = Dog()
+snoopy.bark()
+snoopy.sit()
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,13 @@
-#!/usr/bin/env python3
-
 class Person:
-    # Class body goes here
+    def talk(self):
+        print("Hello, World!")
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking")
+
+# Creating instances of the Person class
+vivian = Person()
+vivian.talk()
+
+aluoch = Person()
+aluoch.walk()
